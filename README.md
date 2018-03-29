418c01344356342060cbded8a91ddd0ac6f61766
Johnny <jmdwestlake@yahoo.co.uk>
2018-03-29 12:00:09 +0100

    Commit Demo

---
+++ README.md
@@ -0,0 +1,11 @@
+# XAML Transform3D Light sub sample
+
+Requies 16299 SDK.
+Tested on Fall Creators Update
+
+## Instructions:
+
+- Run Sample
+- Press "Add Lights". This adds a PointLight too all buttons
+- Press "Depth Animation". This plays a transform3D aniamtion on all items in grid. Note lights have dissapear.
+- Try all combinations of buttons (remove lights, remove 3d transforms, add lights etc) - lights never return
---
+++ Transform3DLightBug/SampleLight.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Transform3DLightBug
+{
+    public class SampleLight : XamlLight
+    {
+        protected override void OnConnected(UIElement newElement)
+        {
+            Compositor compositor = Window.Current.Compositor;
+
+            // Create AmbientLight and set its properties
+            var light = compositor.CreatePointLight();
+            light.Color = Colors.White;
+            light.Intensity = 1.5f;
+            light.Offset = new System.Numerics.Vector3(75);
+
+            // Associate CompositionLight with XamlLight
+            CompositionLight = light;
+
+            // Add UIElement to the Light's Targets
+            SampleLight.AddTargetElement(GetId(), newElement);
+        }
+
+        protected override void OnDisconnected(UIElement oldElement)
+        {
+            // Dispose Light when it is removed from the tree
+            SampleLight.RemoveTargetElement(GetId(), oldElement);
+            CompositionLight.Dispose();
+        }
+
+        protected override string GetId() => typeof(SampleLight).FullName;
+    }
+}
